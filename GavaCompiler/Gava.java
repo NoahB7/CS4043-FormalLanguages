a5c1da15becc364970630356cb941ea1d779cb8d
Noah Buchanan <96553082+NoahB7@users.noreply.github.com>
2021-12-23 16:30:33 -0600

    Add files via upload

---
+++ GavaCompiler/Gava.java
@@ -0,0 +1,14 @@
+public class Gava{
+	public static void main(String[] args){
+		for (int x=3;x<10.0;x+=2.0){
+			if((true!=false)||true&&3.0>2.0&&4.0<6.0){
+				for (int i=1;(i<100.0)&&false!=true;i+=2.0){
+					System.out.println("hello");
+					if(x==9.0){
+						System.out.println("loop is ending");
+					}
+				}
+			}
+		}
+	}
+}
